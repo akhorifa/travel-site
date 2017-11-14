57f10316b3c0ecbb11345de12d1e40e8d05030ec
Abdenour <akhorifa@gmail.com>
2017-11-14 22:34:43 +0100

    Completed large hero block and intro content block.

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
+
+//gulp.task('default', function() {
+//    
+//    console.log("Bravo, you created a Gulp task");
+//    
+//});
+//
+//gulp.task('html', function() {
+//    
+//    console.log("Imagine something useful being done to your html here");
+//    
+//});
