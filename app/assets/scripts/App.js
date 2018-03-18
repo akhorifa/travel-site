6a31bf6c411b3f80bb1b80fbe3a0296c8e036209
Abdenour Khorifa <abdenourkhorifa@abdenours-mbp.home>
2018-03-18 11:08:37 +0100

    Added browser support fallbacks.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -8,7 +8,7 @@ var mobileMenu     = new MobileMenu();
 //var revealOnScroll = new RevealOnScroll();
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
-var stickyHeader = new StickyHeader();
+var stickyHeader = new StickyHeader(); 
 var modal = new Modal();
 
 
